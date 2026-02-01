affb33973cab0143204a81c513c76e171026b285
Sergin-cat <111153675+Sergin-cat@users.noreply.github.com>
2026-02-02 01:06:28 +0200

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,31 @@
+const timerBlock = document.querySelector('.timer__time')
+const deadline = '28 february 2026'
+
+let interval
+
+const updateClock = () => {
+    const date = new Date().getTime();
+    const dateDeadline = new Date("01 february 2026").getTime();
+    const timeRemaining = (dateDeadline - date) / 1000;
+
+    const hours = Math.floor(timeRemaining / 60 / 60);
+    const minutes = Math.floor((timeRemaining / 60) % 60);
+    const seconds = Math.floor(timeRemaining % 60);
+
+    const fHours = hours < 10 ? '0' + hours : hours
+    const fMinutes = minutes < 10 ? "0" + minutes : minutes;
+    const fSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+    timerBlock.textContent = `${fHours}:${fMinutes}:${fSeconds}`
+
+    if (timeRemaining <= 0) {
+        clearInterval(interval)
+        timerBlock.textContent = `00:00:00`
+    }
+}
+
+updateClock() 
+
+interval = setInterval(updateClock, 500)
+
+// 15:40
